6a194acc61ae6f36b4f4bd32500e843aa0563f55
Daniel Schneider <daschne8@gmail.com>
2019-06-17 20:47:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(blockRange){
   return function(start,end){
-    const difference = Math.abs(start-end)
+    const difference = Math.abs(parseInt(start)-parseInt(end))
     const inRange = Math.abs(blockRange - difference)
     if (difference < blockRange) {
       return `within range by ${inRange}`
