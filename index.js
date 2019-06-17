ef600eb2906851d9907c77adcc803b8fc2ce5892
Daniel Schneider <daschne8@gmail.com>
2019-06-17 20:44:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function produceDrivingRange(blockRange){
   return function(start,end){
-    return Math.abs(start-end) < blockRange
+    let difference = Math.abs(start-end)
+    if (difference < blockRange) {
+      let inRange = Math.abs(blockRange - difference)
+      return ``
+    }
   }
 }
